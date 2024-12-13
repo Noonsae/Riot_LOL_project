7860b90ca7b24dc10e7c51384adfc8c6960fdc4e
Noon_sae <minseock7308@gmail.com>
2024-12-14 02:08:20 +0900

    ✨Footer 생성

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-900">
+      <div className="max-w-7xl m-auto p-3">
+        <p className="text-white text-center text-sm md:text-base">
+          @ Copyright 2024 <span className="text-yellow-300">NoonSae.</span> All
+          rights reserved
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
