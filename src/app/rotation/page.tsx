df308f215c442cb23609f153bc51221ab2da0377
Noon_sae <minseock7308@gmail.com>
2024-12-18 13:08:18 +0900

    📝로테이션 페이지 fetch로직에 맞춰 수정

--- src/app/rotation/page.tsx
+++ src/app/rotation/page.tsx
@@ -1,95 +1,81 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import Head from "next/head";
+import { Champion } from "@/types/champions";
+import { useQuery } from "@tanstack/react-query";
+import { fetchChampionRotationData } from "@/utils/rotateApi";
+import { ChampionCard } from "@/components/champion/ChampionCard";
 
-import Image from "next/image";
-import { ChampionDetails } from "@/types/champions";
-import { ChampionRotation } from "@/types/rotation";
+type RotationProps = {
+  allPlayers: Champion[];
+  newPlayers: Champion[];
+};
 
 export default function RotationPage() {
-  // 로테이션 데이터, 로딩 상태, 에러 메시지, 챔피언 데이터를 위한 상태값
-  const [rotationData, setRotationData] = useState<ChampionRotation | null>(
-    null
-  );
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string | null>(null);
-  const [championData, setChampionData] = useState<ChampionDetails | null>(
-    null
-  );
+  const { data, isPending, error, refetch } = useQuery<RotationProps>({
+    queryKey: ["championRotation"],
+    queryFn: fetchChampionRotationData,
+    retry: false,
+    refetchOnWindowFocus: false,
+    staleTime: 1000 * 60 * 5,
+  });
 
-  useEffect(() => {
-    async function fetchRotation() {
-      setLoading(true); // 로딩 상태 시작
-      setError(null); // 기존 에러 초기화
-
-      try {
-        // 1. 로테이션 데이터 가져오기
-        const rotationRes = await fetch("/api/rotation");
-        if (!rotationRes.ok) {
-          throw new Error("로테이션 데이터를 가져오는데 실패했습니다.");
-        }
-        const rotation: ChampionRotation = await rotationRes.json();
-        setRotationData(rotation);
-
-        // 2. 챔피언 데이터 가져오기
-        const championRes = await fetch("/api/championData");
-        if (!championRes.ok) {
-          throw new Error("챔피언 데이터를 가져오는데 실패했습니다.");
-        }
-        const { data: champions }: { data: ChampionDetails } =
-          await championRes.json();
-        setChampionData(champions);
-      } catch (err) {
-        // API 호출 중 발생한 에러 처리
-        setError((err as Error).message);
-      } finally {
-        setLoading(false); // 로딩 상태 종료
-      }
-    }
-    fetchRotation();
-  }, []);
-
-  // 로딩 상태일 때
-  if (loading) {
-    return <p>로딩중임당...^-^)b</p>;
+  if (isPending) {
+    return ("로딩 중 입니당..");
   }
 
-  // 에러 발생 시
   if (error) {
-    return <p>엥? 뭔가 이상한뎅...ㅠ^ㅠ)p</p>;
+    return ("엥.. 뭔가 이상한뎅.. ㅇㅅaㅇ;");
   }
 
-  // 데이터가 없을 경우
-  if (!rotationData || !championData) {
-    return <p>데이터를 찾을 수 없습니다. 다시 시도 해주세요.</p>;
-  }
+  // 데이터가 성공적으로 로드되었을 경우 구조 분해
+  const { allPlayers, newPlayers } = data as RotationProps;
 
-  // 정상적으로 데이터를 불러왔을 경우
   return (
-    <ul>
-      {rotationData?.freeChampionIdsForNewPlayers.map((id) => {
-        // 로테이션 챔피언 ID와 일치하는 챔피언 데이터 찾기
-        const champ = championData
-          ? Object.values(championData).find(
-              (champion) => parseInt(champion.key) === id
-            )
-          : null;
-        if (!champ) return null;
+    <>
+      <Head>
+        <title>금주 로테이션 챔피언</title>
+        <meta
+          name="description"
+          content="Riot Games API를 활용하여 금주의 로테이션 정보를 제공합니다."
+        />
+        <meta property="og:title" content="금주 로테이션 정보" />
+        <meta
+          property="og:description"
+          content="Riot Games API를 활용하여 금주의 로테이션 정보를 제공합니다."
+        />
+        <meta property="og:url" content="http://localhost:3000/rotation" />
+      </Head>
 
-        // 챔피언 정보 렌더링
-        return (
-          <li key={id} className="grid grid-cols-10 gap-4">
-            <Image
-              src={`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champ.id}_0.jpg`}
-              alt={champ.name}
-              width={100}
-              height={100}
-            />
-            <p>{champ.name}</p>
-            <p>{champ.title}</p>
-          </li>
-        );
-      })}
-    </ul>
+      <article className="flex flex-col gap-10 min-h-screen pb-[200px] m-auto max-w-custom container">
+        <div className="mb-5">
+          <div className="text-3xl txt mb-10">
+            <h2 className="text-3xl font-bold">금주 로테이션 챔피언</h2>
+          </div>
+
+          <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-7 gap-4">
+            {allPlayers.map((champion: Champion) => (
+              <ChampionCard key={champion.id} champion={champion} />
+            ))}
+          </div>
+        </div>
+        
+        <hr className="border border-[#ddd] dark:border-[#a1a1a1]"/>
+
+        <div className="mt-5">
+          <div className="txt pb-10">
+            <h2 className="text-3xl font-bold">
+              로테이션 챔피언 (신규)
+            </h2>
+          </div>
+
+          <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-7 gap-4">
+            {newPlayers.map((champion: Champion) => (
+              <ChampionCard key={champion.id} champion={champion} />
+            ))}
+          </div>
+        </div>
+      </article>
+    </>
   );
 }
