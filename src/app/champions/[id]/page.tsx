04d3d9acc7678c8ecda8f561172fda1cfde9f145
Noon_sae <minseock7308@gmail.com>
2024-12-19 06:36:50 +0900

    ♻️ 챔피언 디테일 서버 컴포넌트 리펙토링

--- src/app/champions/[id]/page.tsx
+++ src/app/champions/[id]/page.tsx
@@ -1,9 +1,7 @@
 import { Metadata } from "next";
-
 import { ChampionDetails } from "@/types/champions";
 
 import { fetchChampionDetails, fetchLatestVersion } from "@/utils/serverApi";
-
 import Detail from "./Details";
 
 type Props = {
@@ -12,7 +10,9 @@ type Props = {
   };
 };
 
-export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
   const champion: ChampionDetails = await fetchChampionDetails(params.id);
   return {
     title: `League Of Legends : ${champion.name}`,
@@ -20,21 +20,25 @@ export const generateMetadata = async ({ params }: Props): Promise<Metadata> => 
     openGraph: {
       title: `League Of Legends : ${champion.name}`,
       description: `${champion.lore}`,
-      url: `https://lol-app-psi.vercel.app//champions/${params.id}`,
+      url: `https://lol-app-psi.vercel.app/champions/${params.id}`,
     },
   };
-}
+};
+
+// 서버에서 데이터를 가져오는 비동기 함수
+const fetchData = async (id: string) => {
+  const [version, champion] = await Promise.all([
+    fetchLatestVersion(),
+    fetchChampionDetails(id),
+  ]);
+  return { version, champion };
+};
 
 const DetailPage = async ({ params }: Props) => {
+  // 서버에서 데이터를 가져옴
+  const { version, champion } = await fetchData(params.id);
 
-  const version: string = await fetchLatestVersion();
-  const champion: ChampionDetails = await fetchChampionDetails(params.id);
-
-  return (
-    
-      <Detail champion={champion} version={version} />
-    
-  );
-}
+  return <Detail champion={champion} version={version} />;
+};
 
 export default DetailPage;
