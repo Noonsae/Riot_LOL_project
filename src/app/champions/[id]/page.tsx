0c4d2d2bee569ccea2809f54e8945f1a8a06f4e8
Noon_sae <minseock7308@gmail.com>
2024-12-18 13:07:08 +0900

    ✨챔피언 서버 컴포넌트 생성

--- src/app/champions/[id]/page.tsx
+++ src/app/champions/[id]/page.tsx
@@ -1,57 +1,30 @@
+import { Metadata } from "next";
 import { ChampionDetails } from "@/types/champions";
-import fetchChampionData from "@/utils/serverApi";
-import Image from "next/image";
+import { fetchChampionDetails, fetchLatestVersion } from "@/utils/serverApi";
+import Detail from "./Details";
 
-interface ChampionPageProps {
+type Props = {
   params: {
     id: string;
   };
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const champion: ChampionDetails = await fetchChampionDetails(params.id);
+  return {
+    title: `League Of Legends : ${champion.name}`,
+    description: `${champion.lore}`,
+    openGraph: {
+      title: `League Of Legends : ${champion.name}`,
+      description: `${champion.lore}`,
+      url: `https://lol-app-psi.vercel.app//champions/${params.id}`,
+    },
+  };
 }
 
-export default async function ChampionPage({ params }: ChampionPageProps) {
-  const { id } = params;
+export default async function DetailPage({ params }: Props) {
+  const version: string = await fetchLatestVersion();
+  const champion: ChampionDetails = await fetchChampionDetails(params.id);
 
-  try {
-    // 데이터 가져오기
-    const { data: champions, version } = await fetchChampionData();
-
-    // 챔피언 찾기
-    const champion: ChampionDetails | undefined = champions[id];
-
-    if (!champion) {
-      console.error(`Champion with id ${id} not found.`);
-      return <div>해당 챔피언 정보를 찾을 수 없습니다.</div>;
-    }
-
-    // 이미지 URL 생성
-    const imageUrl = `https://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${champion.image.full}`;
-
-    // 디버깅: 로그 확인
-    console.log("Champion:", champion);
-    console.log("Image URL:", imageUrl);
-
-    // 페이지 렌더링
-    return (
-      <div className="flex justify-center">
-        <div className="p-4 bg-black h-[500px] w-[700px] mt-11">
-          <div className="h-[100px] w-[100px] bg-white">
-            <h1 className="text-2xl text-black font-bold">{champion.name}</h1>
-            <h2 className="text-lg text-gray-600">{champion.title}</h2>
-          </div>
-          <div className="flex gap-4 mt-4">
-            <Image
-              src={imageUrl}
-              alt={champion.name}
-              width={200}
-              height={200}
-            />
-            <p className="text-white">{champion.blurb}</p>
-          </div>
-        </div>
-      </div>
-    );
-  } catch (error) {
-    console.error("Error fetching champion data:", error);
-    return <div>오류가 발생했습니다. 다시 시도해주세요.</div>;
-  }
+  return <Detail champion={champion} version={version} />;
 }
