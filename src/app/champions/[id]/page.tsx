119ca989d342e1cfa869220670cc2847e699b8da
Noon_sae <minseock7308@gmail.com>
2024-12-19 01:51:56 +0900

    ♻️ ChampionDetails 페이지 함수 표현식으로 리펙토링

--- src/app/champions/[id]/page.tsx
+++ src/app/champions/[id]/page.tsx
@@ -1,6 +1,9 @@
 import { Metadata } from "next";
+
 import { ChampionDetails } from "@/types/champions";
+
 import { fetchChampionDetails, fetchLatestVersion } from "@/utils/serverApi";
+
 import Detail from "./Details";
 
 type Props = {
@@ -9,7 +12,7 @@ type Props = {
   };
 };
 
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
   const champion: ChampionDetails = await fetchChampionDetails(params.id);
   return {
     title: `League Of Legends : ${champion.name}`,
@@ -22,9 +25,16 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   };
 }
 
-export default async function DetailPage({ params }: Props) {
+const DetailPage = async ({ params }: Props) => {
+
   const version: string = await fetchLatestVersion();
   const champion: ChampionDetails = await fetchChampionDetails(params.id);
 
-  return <Detail champion={champion} version={version} />;
+  return (
+    
+      <Detail champion={champion} version={version} />
+    
+  );
 }
+
+export default DetailPage;
