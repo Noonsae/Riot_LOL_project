e85dbc9799b28c74e4bac1f9e3007cf0db1c4046
Noon_sae <minseock7308@gmail.com>
2024-12-18 12:45:55 +0900

    📝items 타입 일부 수정

--- src/types/items.ts
+++ src/types/items.ts
@@ -1,21 +1,27 @@
-// 아이템 데이터를 나타내는 타입
-export interface ItemDetails {
+import { Image } from "./champions";
+
+// 아이템 기본 정보를 나타내는 타입
+export type ItemDetails = {
+  id: string;
   name: string;
-  description: string;
-  plaintext?: string;
-  image?: {
-    full?: string;
-    sprite: string;
-    group: string;
-    x: number;
-    y: number;
-    w: number;
-    h: number;
-  };
-  gold?: {
+  description: string; // 아이템 설명 (HTML 형식)
+  plaintext: string; // 아이템 설명 (간단한 텍스트)
+  into: string[]; // 해당 아이템으로 업그레이드되는 아이템 ID 목록
+  image: Image;
+  gold: {
     base: number;
+    total: number;
+    sell: number;
     purchasable: boolean;
-    total?: number;
-    sell?: number;
   };
-}
+  tags: string[]; // 아이템 유형 태그 (예: Boots)
+};
+
+// 아이템 목록 응답 형태를 나타내는 타입
+export type ItemListResponse = {
+  type: string;
+  version: string;
+  data: {
+    [key: string]: ItemDetails;
+  };
+};
