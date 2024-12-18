0acc97876cb74bf81395980683fc93733d7bcae7
Noon_sae <minseock7308@gmail.com>
2024-12-19 06:34:21 +0900

    📝로딩 페이지 수정

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,11 +1,13 @@
+"use client"
+
 import { Loader2 } from "lucide-react";
 
 const Loading = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="-mt-[100px] flex items-center justify-center min-h-screen">
       <Loader2 className="animate-spin w-10 h-10" />
     </div>
   );
-}
+};
 
 export default Loading;
