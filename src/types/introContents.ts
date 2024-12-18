0976ee7a368ec260ff7b4d60ef6256a081d41b8a
Noon_sae <minseock7308@gmail.com>
2024-12-19 01:23:13 +0900

    ✨introContents 타입 생성

---
+++ src/types/introContents.ts
@@ -0,0 +1,19 @@
+export type introContents = {
+  title: string;
+  description?: string;
+  icon_name_01?: string;
+  icon_name_02?: string;
+  icon_name_03?: string;
+  icon_name_04?: string;
+  icon_name_05?: string;
+  icon_description_01?: string;
+  icon_description_02?: string;
+  icon_description_03?: string;
+  icon_description_04?: string;
+  icon_description_05?: string;
+  btn_img_01?: string;
+  btn_img_02?: string;
+  btn_img_03?: string;
+  btn_img_04?: string;
+  btn_img_05?: string;
+};
