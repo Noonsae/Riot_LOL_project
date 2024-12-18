0ea044b318cb1bf0ec387c5c131431a720ff89e4
Noon_sae <minseock7308@gmail.com>
2024-12-18 12:36:50 +0900

    ✨ Inner 생성

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,16 +6,23 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // 추가된 내용
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        background: {
+          light: "#F5F5F5",
+          dark: "#1a202c",
+        },
+        darkTrack: "#1e1e1e", // 다크 모드 트랙 색상
+        darkThumb: "#3a3a3a", // 다크 모드 핸들 색상
+        lightTrack: "#e2e8f0", // 라이트 모드 트랙 색상
+        lightThumb: "#a0aec0", // 라이트 모드 핸들 색상
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-textshadow")], // 플러그인 유지
 };
+
 export default config;
