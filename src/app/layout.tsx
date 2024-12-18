edefb138b1bfab197718e9bf1c1a8cf3059e9161
Noon_sae <minseock7308@gmail.com>
2024-12-18 12:46:58 +0900

    ✨ layout생성 및 프로바이더 연결

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,37 @@
-"use client";
+import type { Metadata } from "next";
+import Header from "@/components/ui/Header";
+import "@/styles/globals.css";
 
-import Header from "@/components/layout/Header";
-import Footer from "@/components/layout/Footer";
+import { ThemeProvider } from "next-themes";
+import MyProvider from "./providers/Provider";
+import Footer from "@/components/ui/Footer";
 
-import "./globals.css";
-
-import useDarkMode from "@/utils/useDarkMode";
+export const metadata: Metadata = {
+  title: "LoLa",
+  description: "Riot API를 활용한 리그 오브 레전드 정보 앱 만들기",
+};
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
-  const { theme, toggleTheme } = useDarkMode();
-
+}>) {
   return (
-    <html lang="ko" className={theme}>
-      <body className={theme}>
-        <Header theme={theme} toggleTheme={toggleTheme} />
-
-        {/* 페이지 콘텐츠 */}
-        <main className="container mx-auto mt-4">{children}</main>
-
-        <Footer />
-      </body>
+    <html lang="ko">
+      <link rel="icon" href="/icons/bee.png" sizes="any" />
+      <MyProvider>
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          <body className="dark:bg-[#1a1a1a]">
+            <Header />
+            <div className="inner mt-[80px]">
+              <main className="min-h-[calc(100vh-200px)] pt-[80px]">
+                {children}
+              </main>
+            </div>
+            <Footer />
+          </body>
+        </ThemeProvider>
+      </MyProvider>
     </html>
   );
 }
